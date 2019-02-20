97229541e0a2978f4957734011549de771bc2742
Soma Mbadiwe <somasystemsng@gmail.com>
2019-02-20 16:58:08 -0500

    First commit. Lots of bugs from orginal repo fixed

---
+++ run_prepare_data.sh
@@ -0,0 +1,16 @@
+source ~/.bashrc
+
+if [ ! -d ./data/DF_img_pose ]; then
+	if [ ! -d ./data]; then
+		mkdir data
+	fi
+    cd data
+    wget http://homes.esat.kuleuven.be/~liqianma/NIPS17_PG2/data/DF_img_pose.zip
+    wget homes.esat.kuleuven.be/~liqianma/NIPS17_PG2/data/DF_img_pose.zip
+    unzip DF_img_pose.zip
+    rm -f DF_img_pose.zip
+    cd -
+fi
+
+python ./prepare_data.py './data/DF_img_pose' 'train'
+python ./prepare_data.py './data/DF_img_pose' 'test'
